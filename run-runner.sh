b11171d7f837b2a4008d085a424bf63cd1d026a5
Bill Meyer <thebillmeyer@gmail.com>
2020-05-13 16:19:17 -0500

    Latest device updates

--- run-runner.sh
+++ run-runner.sh
@@ -2,7 +2,8 @@
 
 APPIPA=LoanCalc.ipa
 TESTIPA=LoanCalcUITests-Runner.ipa
-DEVICE=iPhone_6S_carrier_test_sjc
+#DEVICE=iPhone_XS_Max_POC168
+DEVICE=iPhone_6s_Plus_13_real
 
 #runner.sh config --path runner.yml --apikey ${LOANCALC_IOS_APIKEY} $*
-runner.sh xcuitest --test ${TESTIPA} --app ${APPIPA} --apikey ${LOANCALC_IOS_APIKEY} --datacenter US --device ${DEVICE} --testsToRun LoanCalcUITests/testLoanCalc1,LoanCalcUITests/testLoanCalc2 $*
+runner.sh xcuitest --testname "LoanCalc\ XCUITests" --test ${TESTIPA} --app ${APPIPA} --apikey ${LOANCALC_IOS_APIKEY} --datacenter US --device ${DEVICE} --testsToRun LoanCalcUITests/testLoanCalc1,LoanCalcUITests/testLoanCalc2 $*
--- test-local.sh
+++ test-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild -scheme LoanCalc -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' test-without-building $*
+xcodebuild -scheme LoanCalc -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' test-without-building $*
