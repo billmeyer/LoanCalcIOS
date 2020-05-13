5c6a210162de23cba72456e761d7e98987053d68
Bill Meyer <thebillmeyer@gmail.com>
2020-05-13 16:22:00 -0500

    Added runner.jar to the project

--- run-runner.sh
+++ run-runner.sh
@@ -5,5 +5,4 @@ TESTIPA=LoanCalcUITests-Runner.ipa
 #DEVICE=iPhone_XS_Max_POC168
 DEVICE=iPhone_6s_Plus_13_real
 
-#runner.sh config --path runner.yml --apikey ${LOANCALC_IOS_APIKEY} $*
-runner.sh xcuitest --testname "LoanCalc\ XCUITests" --test ${TESTIPA} --app ${APPIPA} --apikey ${LOANCALC_IOS_APIKEY} --datacenter US --device ${DEVICE} --testsToRun LoanCalcUITests/testLoanCalc1,LoanCalcUITests/testLoanCalc2 $*
+java -jar runner.jar xcuitest --testname "LoanCalc\ XCUITests" --test ${TESTIPA} --app ${APPIPA} --apikey ${LOANCALC_IOS_APIKEY} --datacenter US --device ${DEVICE} --testsToRun LoanCalcUITests/testLoanCalc1,LoanCalcUITests/testLoanCalc2 $*
